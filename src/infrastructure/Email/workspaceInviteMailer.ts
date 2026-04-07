ddf83d5bd159b9a3bd96204ccf04068366c5bdac
Zafra Ziaudeen <zafraziaudeen@gmail.com>
2026-04-07 11:01:33 +0530

    add a family for smtp

--- src/infrastructure/Email/workspaceInviteMailer.ts
+++ src/infrastructure/Email/workspaceInviteMailer.ts
@@ -21,16 +21,16 @@ const getTransporter = (): Transporter => {
     return transporter;
   }
 
-  transporter = nodemailer.createTransport({
-    host: env.SMTP_HOST,
-    port: env.SMTP_PORT,
-    secure: env.SMTP_PORT === 465,
-    auth: {
-      user: env.SMTP_USER,
-      pass: env.SMTP_PASS,
-    },
-  });
-
+ transporter = nodemailer.createTransport({
+  host: env.SMTP_HOST,
+  port: env.SMTP_PORT,
+  secure: env.SMTP_PORT === 465,
+  family: 4,   
+  auth: {
+    user: env.SMTP_USER,
+    pass: env.SMTP_PASS,
+  },
+});
   return transporter;
 };
 
